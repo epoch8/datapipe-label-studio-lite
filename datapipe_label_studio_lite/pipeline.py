158adc52b08f3a8d90731631e97b412ce1fea34f
Alexander Kozlov <alexander.kozlovsky.m@gmail.com>
2023-01-17 13:04:06 +0300

    fix response

--- datapipe_label_studio_lite/pipeline.py
+++ datapipe_label_studio_lite/pipeline.py
@@ -1,5 +1,6 @@
 import numpy as np
 import pandas as pd
+import requests
 from typing import Any, Union, List, Optional
 from datetime import datetime
 from dataclasses import dataclass
@@ -146,7 +147,10 @@ class LabelStudioStep(PipelineStep):
         )
         catalog.add_datatable(self.output, Table(output_dt.table_store))
 
-        def upload_tasks(df: pd.DataFrame):
+        def upload_tasks(
+            df: pd.DataFrame,
+            df_uploader: pd.DataFrame
+        ):
             """
                 Добавляет в LS новые задачи с заданными ключами.
                 (Не поддерживает удаление задач, если в input они пропадают)
@@ -154,22 +158,20 @@ class LabelStudioStep(PipelineStep):
             if df.empty or len(df) == 0:
                 return
 
+            df_merge_uploader = pd.merge(df, df_uploader, on=[self.primary_keys])
             # Удаляем существующие задачи и перезаливаем их
-            df_idx = data_to_index(df, self.primary_keys)
-            existing_tasks_df_without_annotations = input_uploader_dt.get_data(idx=df_idx)
-            if len(existing_tasks_df_without_annotations) > 0:
-                existing_idx = data_to_index(existing_tasks_df_without_annotations, self.primary_keys)
-                df_to_be_deleted = pd.merge(
-                    left=index_to_data(df, existing_idx),
-                    right=existing_tasks_df_without_annotations[self.primary_keys + ['task_id']],
-                    on=self.primary_keys
-                )
-                for task_id in df_to_be_deleted['task_id']:
-                    if int(task_id) == -1:
-                        continue
-                    self.project.make_request(
-                        method='DELETE', url=f"api/tasks/{task_id}/",
+            if len(df_merge_uploader) > 0:
+                for task_id in df_merge_uploader['task_id']:
+                    response = self.project.session.request(
+                        method='DELETE', url=self.project.get_url(f"api/tasks/{task_id}/"),
+                        headers=self.project.headers, cookies=self.project.cookies
                     )
+                    if response.status_code in [204]:
+                        pass
+                    elif response.status_code in [404]:
+                        pass
+                    else:
+                        response.raise_for_status()
 
             # Добавляем новые задачи
             data_to_be_added = [
@@ -241,7 +243,7 @@ class LabelStudioStep(PipelineStep):
             BatchTransformStep(
                 name='upload_data_to_ls',
                 func=upload_tasks,
-                input_dts=[input_dt],
+                input_dts=[input_dt, input_uploader_dt],
                 output_dts=[input_uploader_dt],
             ),
             DatatableTransformStepNoChangeList(
