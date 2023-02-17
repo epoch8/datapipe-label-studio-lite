47cb9fd64c76ad3dde1a8a39d8656422e3827aed
Andrey Tatarinov <a@tatarinov.co>
2023-02-18 02:25:13 +0400

    *

--- datapipe_label_studio_lite/pipeline.py
+++ datapipe_label_studio_lite/pipeline.py
@@ -32,8 +32,8 @@ class DatatableTransformStepNoChangeList(DatatableTransformStep):
 
 @dataclass
 class LabelStudioStep(PipelineStep):
-    input: str # Input Table name
-    output: str # Output Table name
+    input: str  # Input Table name
+    output: str  # Output Table name
     sync_table: str
 
     ls_url: str
